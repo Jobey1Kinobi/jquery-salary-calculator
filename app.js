199c23961a790bc299ca35771a1fb7c0caa7101f
Jobey1Kinobi <jobers92@gmail.com>
2019-04-19 18:41:36 -0500

    update/add

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('app.js');
+//--------------------\\
+$(document).ready(function() {
+    $('#employeeSubmit').on('submit', function(event){
+        event.preventDefault()
+    })
+    $('')
+});
+
+
+
