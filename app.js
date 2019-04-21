648acff88413d118bf27c3e69cebfffa34a8adaa
Jobey1Kinobi <jobers92@gmail.com>
2019-04-21 00:34:57 -0500

    update

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('app.js');
+
+$( document ).ready( functOne );
+
+
+
+function functOne{
+    console.log('JQ');
+    $('#submitBtn').on( 'click', employeeIn )
+}
+
+function employeeIn(){
+    let firstName = $('')
+    console.log('weeknd');
+}
